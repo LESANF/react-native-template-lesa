50f5d0a4c8a82793915dfc08159fdc31aadcdd34
LESANF <nagong1000@naver.com>
2026-06-29 12:42:11 +0900

    feat: UI 키트 기반 토큰과 SVG 설정 정리

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,17 @@ const { withUniwindConfig } = require('uniwind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+};
+
+config.resolver = {
+  ...config.resolver,
+  assetExts: config.resolver.assetExts.filter((ext) => ext !== 'svg'),
+  sourceExts: [...config.resolver.sourceExts, 'svg'],
+};
+
 module.exports = withUniwindConfig(config, {
   cssEntryFile: './src/global.css',
 });
